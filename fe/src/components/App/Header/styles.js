f0d20794b7400458c47e6041fd2e91a22afe1339
PedroVictorfflank <pedro@fflank.org>
2023-03-07 19:38:17 -0300

    up

--- fe/src/components/App/Header/styles.js
+++ fe/src/components/App/Header/styles.js
@@ -13,7 +13,18 @@ export const InputSearchContainer = styled.div`
   margin-top: 48px;
   width: 100%;
 
+
   input {
+     box-shadow: 0px 4px 10px rgba(0,0,0,0.4);
     width: 100%;
+    background: #fff;
+    border: none;
+    border-radius: 2rem;
+    outline: 0;
+    padding: 0 16px;
+    height: 50px;
+    &::placeholder {
+      color: #BCBCBC;
+    }
   }
 `;
