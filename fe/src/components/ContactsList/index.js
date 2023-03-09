b9f5f687d7bfd7318125ef3c8489ea45510cd5fe
PedroVictor022 <pedro20002victor@gmail.com>
2023-03-08 22:51:41 -0300

    List contacts

--- fe/src/components/ContactsList/index.js
+++ fe/src/components/ContactsList/index.js
@@ -1,4 +1,11 @@
-import { Container, Header } from './styles';
+import {
+  Card,
+  Container, Header, ListContainer,
+} from './styles';
+
+import arrow from '../../assets/images/arrow.svg';
+import edit from '../../assets/images/x.svg';
+import trash from '../../assets/images/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +14,35 @@ export default function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo Contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Pedro Victor</strong>
+              <small>instagram</small>
+            </div>
+            <span>teste@gmail.com</span>
+            <span>31 9999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+
     </Container>
   );
 }
--- fe/src/components/ContactsList/styles.js
+++ fe/src/components/ContactsList/styles.js
@@ -29,3 +29,44 @@ export const Header = styled.div`
     }
   }
 `;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+  header {
+    margin-bottom: 8px;
+    button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
+  }
+    span {
+    margin-right: 8px;
+    font-weight: bold;
+    color : ${({ theme }) => theme.colors.primary.main};
+  }
+  }
+`;
+
+export const Card = styled.div`
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
+  padding: 16px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+      small {
+         background: ${({ theme }) => theme.colors.primary.lighter};
+         color: ${({ theme }) => theme.colors.primary.main};
+         font-weight: bold;
+         text-transform: uppercase;
+      }
+    }
+  }
+`;
